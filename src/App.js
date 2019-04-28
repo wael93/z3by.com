b09229a939b83a9ed1d11b96979e29fcadbe894e
z3by <zoaby.am@gmail.com>
2019-04-28 22:27:01 +0300

    create simple navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import './style.scss';
+import Main from './components/Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>AHMAD MOSTAFA AL-ZOUBI</h1>
-          <h3>Full Stack Software Engineer @BriteCore</h3>
-          <a href="/cv" className="App-link">CV</a>
-        </header>
+        <Main></Main>
       </div>
     );
   }
